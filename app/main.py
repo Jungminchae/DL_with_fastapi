0b3f956d424c3048eacc2b73f6a0a44bca79d3d1
Jungminchae <Jungminchae>
2021-09-08 14:41:21 +0000

    add CORS

--- app/main.py
+++ app/main.py
@@ -6,8 +6,20 @@ import numpy as np
 from tensorflow.keras.models import load_model
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 model_path = "./model/efn_b0_224_na_5-0.43-0.90.h5"
 
@@ -23,13 +35,17 @@ async def classification(place: UploadFile = File(...)):
     with open(upload_path, "wb") as f:
         f.write(image)
     place_image = Image.open(BytesIO(image))
+    
     # resize
     place_image = place_image.resize((224, 224))
+    
     # model load
     model = load_model(model_path)
+    
     # label load
     with open("./model/place_55_label.json", "r", encoding="utf-8-sig") as f:
         label_info = json.load(f)
+    
     # prediction
     place_image = np.array(place_image)
     place_image = place_image[np.newaxis, ...]
